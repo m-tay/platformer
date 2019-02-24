dc3839fd5bcd5bc20fe4ff448bd5ad936355ce12
M Taylor <ecstatic@gmail.com>
2019-02-24 22:11:48 +0000

    Implemented moving platforms

--- platformer/main.cpp
+++ platformer/main.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -39,7 +40,7 @@ float camY = 0.0f;
 
 // game variables
 string levelMap;		// stores level tilemap
-GLuint tileTextures[1];	// stores level tile textures
+GLuint tileTextures[2];	// stores level tile textures
 GLuint bgTexture[1];	// stores background textures
 GLuint playerSprite[3];	// stores player sprite
 
@@ -54,6 +55,169 @@ void update();				//called in winmain to update variables
 // game function prototypes
 void drawLevel();
 char getTile(int x, int y);
+
+class MovingPlatform {
+public:
+	float posX;	// position
+	float posY;
+	float velX = 0.0f;	// velocity
+	float velY = 0.0f;
+	float newPosX;
+	float newPosY;
+	char tileTL;	// holds the 4 tiles around the entity
+	char tileTR;
+	char tileBL;
+	char tileBR;
+	int tilesWide = 3;
+	int platformWidth = tilesWide * tileWidth; // how many tiles wide the platform is
+	bool movingPosX = true;
+
+
+	// constructor
+	MovingPlatform(float x, float y) {
+		posX = x;
+		posY = y;
+	}
+
+	// calculates the tiles around the entity
+	void findSurroundingTiles() {
+		// calculate x,y coords of surrounding tiles
+		int tileLeft = (int)newPosX / tileWidth;
+		int tileRight = (int)(newPosX / tileWidth) + platformWidth;
+		int tileTop = (int)(newPosY / tileHeight) + platformWidth;
+		int tileBottom = (int)newPosY / tileHeight;
+
+		// get the actual tile values from tilemap
+		char tileTL = getTile(tileLeft, tileTop);
+		char tileTR = getTile(tileRight, tileTop);
+		char tileBL = getTile(tileLeft, tileBottom);
+		char tileBR = getTile(tileRight, tileBottom);
+	}
+
+	// check for collisions with tilemap on x axis
+	bool isCollidingX() {
+		// calculate x,y coords of surrounding tiles
+		// assumes entities are no bigger than 1 standard tile width
+		int tileLeft = (int)newPosX / tileWidth;
+		int tileRight = (int)(newPosX / tileWidth) + tilesWide;
+		int tileTop = (int)(posY / tileHeight) + 1;
+		int tileBottom = (int)posY / tileHeight;
+
+		// get the actual tile values from tilemap
+		char tileTL = getTile(tileLeft, tileTop);
+		char tileTR = getTile(tileRight, tileTop);
+		char tileBL = getTile(tileLeft, tileBottom);
+		char tileBR = getTile(tileRight, tileBottom);
+
+		// if any collisions detected...
+		if (tileTL == '#' || tileTR == '#' || tileBL == '#' || tileBR == '#' ||
+			tileTL == 'D' || tileTR == 'D' || tileBL == 'D' || tileBR == 'D') {
+
+			// invert direction of movement flag
+			if (movingPosX)
+				movingPosX = false;
+			else
+				movingPosX = true;
+			
+			return true;
+		}
+
+		// if not colliding, return false
+		return false;
+	}
+
+	// check for collisions with tilemap on y axis
+	bool isCollidingY() {
+		// calculate x,y coords of surrounding tiles
+		// assumes entities are no bigger than 1 standard tile width
+		int tileLeft = (int)posX / tileWidth;
+		int tileRight = (int)(posX / tileWidth) + 1;
+		int tileTop = (int)(newPosY / tileHeight) + 1;
+		int tileBottom = (int)newPosY / tileHeight;
+
+		// get the actual tile values from tilemap
+		char tileTL = getTile(tileLeft, tileTop);
+		char tileTR = getTile(tileRight, tileTop);
+		char tileBL = getTile(tileLeft, tileBottom);
+		char tileBR = getTile(tileRight, tileBottom);
+
+		// ceiling collision, set y velocity to 0
+		if (tileTL == '#' || tileTR == '#' ||
+			tileTL == 'D' || tileTR == 'D') {
+			velY = 0;
+			return true;
+		}
+
+		// ground collision, set jumping flag back to false
+		if (tileBL == '#' || tileBR == '#' ||
+			tileBL == 'D' || tileBR == 'D') {
+			return true;
+		}
+
+		// if not colliding, return false
+		return false;
+	}
+	
+	// update entity position based on velocity
+	void updatePosition() {
+		// add velocity to platform
+		if (movingPosX)
+			velX = 0.2f;
+		else
+			velX = -0.2f;
+		
+		// update entity's model of the tiles surrounding it
+		findSurroundingTiles();
+
+		// calculate proposed new position
+		newPosX = posX + velX;
+		newPosY = posY + velY;
+
+		// check for collision in x axis
+		if (!isCollidingX())
+			posX = newPosX;
+
+		// check for collision in y asis
+		if (!isCollidingY())
+			posY = newPosY;
+	
+	}
+
+	// draws the entity
+	void draw() {
+
+		updatePosition();
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, tileTextures[0]);
+
+		glBegin(GL_QUADS);
+
+		glTexCoord2d(0.0, 1.0);
+		glVertex2f(posX, posY);
+
+		glTexCoord2d(tilesWide, 1.0);
+		glVertex2f(posX + platformWidth, posY);
+
+		glTexCoord2d(tilesWide, 0.0);
+		glVertex2f(posX + platformWidth, posY + tileHeight);
+
+		glTexCoord2d(0.0, 0.0);
+		glVertex2f(posX, posY + tileHeight);
+		glEnd();
+
+
+
+
+		glDisable(GL_TEXTURE_2D);
+	}
+};
+
+// create moving platform object
+MovingPlatform platform1(96.0f, 96.0f);
+
+// make a vector collection of moving platforms, so they can be collision checked
+vector<MovingPlatform*> movingPlatforms;
 
 // object class for all moving objects (ie. things that can move + have collisions 
 class Entity {	
@@ -110,7 +274,8 @@ class Entity {
 			char tileBR = getTile(tileRight, tileBottom);
 
 			// if any collisions detected, return true
-			if (tileTL == '#' || tileTR == '#' || tileBL == '#' || tileBR == '#')
+			if (tileTL == '#' || tileTR == '#' || tileBL == '#' || tileBR == '#' ||
+				tileTL == 'D' || tileTR == 'D' || tileBL == 'D' || tileBR == 'D')
 				return true;
 
 			// if not colliding, return false
@@ -133,14 +298,14 @@ class Entity {
 			char tileBR = getTile(tileRight, tileBottom);
 
 			// ceiling collision, set y velocity to 0
-			if (tileTL == '#' || tileTR == '#') {
+			if (tileTL == '#' || tileTR == '#' || tileTL == 'D' || tileTR == 'D') {
 				velY = 0;
 				return true;
 			}
 
 			// ground collision, set jumping flag back to false
-			if (tileBL == '#' || tileBR == '#') {
-				onGround = false;
+			if (tileBL == '#' || tileBR == '#' || tileBL == 'D' || tileBR == 'D') {
+				onGround = true;
 				return true;
 			}
 
@@ -148,7 +313,55 @@ class Entity {
 			return false;
 		}
 
+		bool isCollidingWithMovingPlatform() {			
+			// loop through all moving platforms
+			for (int i = 0; i < movingPlatforms.size(); i++) {
+				// AABB edge overlap checks
+				if (newPosX + tileWidth >= movingPlatforms.at(i)->posX								&&
+					newPosX <= movingPlatforms.at(i)->posX + movingPlatforms.at(i)->platformWidth	&&
+					newPosY + tileHeight >= movingPlatforms.at(i)->posY							&&
+					newPosY <= movingPlatforms.at(i)->posY + tileHeight) {
 
+					// if AABB overlap, check if entity is hitting it from the bottom
+					if ((newPosX + tileHeight) >= movingPlatforms.at(i)->posX &&
+						(newPosX + tileHeight) < (movingPlatforms.at(i)->posX + tileHeight)) {
+						velY = 0;
+					}
+		
+					return true;
+				}
+			}
+
+			// if no edge overlaps detected
+			return false;
+		}
+
+		void trackMovingPlatforms() {
+
+			// loop through all moving platforms
+			for (int i = 0; i < movingPlatforms.size(); i++) {
+
+				// check if within x axis of moving platform
+				if (posX >= movingPlatforms.at(i)->posX && (posX + tileWidth) < (movingPlatforms.at(i)->posX + movingPlatforms.at(i)->platformWidth)) {
+
+					// check if close enough to surface of moving platform to track movement
+					if ((movingPlatforms.at(i)->posY - posY + 32) > -0.3f && (movingPlatforms.at(i)->posY - posY + 32) < 0.3f) {
+						posX += movingPlatforms.at(i)->velX;
+						velY = 0;
+						onGround = true;
+
+					}
+				}
+				// if out of bounds of moving platform and was on ground
+				else {
+					if (onGround) {
+						velY -= gravityRate;
+					}
+				}
+			}
+		}
+
+		
 		// update entity position based on velocity
 		void updatePosition() {
 			// update entity's model of the tiles surrounding it
@@ -158,18 +371,21 @@ class Entity {
 			newPosX = posX + velX;
 			newPosY = posY + velY;
 
-			// check for collision in x axis
-			if (!isCollidingX())
+			// check for collision with tilemap in x axis
+			if (!isCollidingX() && !isCollidingWithMovingPlatform())
 				posX = newPosX;
 
-			if (!isCollidingY())
+			// check for collision in with tilemap y axis
+			if (!isCollidingY() && !isCollidingWithMovingPlatform())
 				posY = newPosY;
 
 			// deceleration on x axis
 			velX = 0;
 
+			trackMovingPlatforms();
+
 			// apply gravity 
-			if(onGround)
+			if(!onGround)
 				velY -= gravityRate;
 
 
@@ -192,21 +408,26 @@ class Entity {
 				glVertex2f(posX, posY);
 
 				glTexCoord2d(1.0, 1.0);
-				glVertex2f(posX + tileHeight, posY);
+				glVertex2f(posX + tileWidth, posY);
 				
 				glTexCoord2d(1.0, 0.0);
-				glVertex2f(posX + tileHeight, posY + tileWidth);
+				glVertex2f(posX + tileWidth, posY + tileHeight);
 				
 				glTexCoord2d(0.0, 0.0);
-				glVertex2f(posX, posY + tileWidth);
+				glVertex2f(posX, posY + tileHeight);
 			glEnd();
 
 			glDisable(GL_TEXTURE_2D);
 		}
 };
 
+
+
+
 // create player entity object
 Entity playerEntity(32.0f, 32.0f);
+
+
 
 // texture loader
 int loadTileTextures()
@@ -215,6 +436,11 @@ int loadTileTextures()
 	tileTextures[0] = SOIL_load_OGL_texture
 	(
 		"textures/tiles/GrassCliffMid.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+
+	// loads image directly as texture
+	tileTextures[1] = SOIL_load_OGL_texture
+	(
+		"textures/tiles/Dirt.png", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
 
 	bgTexture[0] = SOIL_load_OGL_texture
 	(
@@ -306,6 +532,8 @@ void display()
 	playerEntity.draw();
 	playerEntity.frame += 0.01f;	// update frame animation
 
+	platform1.draw();
+
 	glPopMatrix();
 
 	glFlush();
@@ -350,29 +578,34 @@ void drawLevel() {
 		for (int y = 0; y < levelHeight; y++) {
 			char tile = getTile(x, y);
 
-			if (tile == '#') {
-				// enable and bind texture
+			if (tile == '#' || tile == 'D') {
+
+				// enable and bind texture relevant texture
 				glEnable(GL_TEXTURE_2D);
-				glBindTexture(GL_TEXTURE_2D, tileTextures[0]); 
+				if (tile == '#') { // grass tile
+					glBindTexture(GL_TEXTURE_2D, tileTextures[0]);
+				}
+				if (tile == 'D') { // dirt tile
+					glBindTexture(GL_TEXTURE_2D, tileTextures[1]);
+				}
 
 				glBegin(GL_POLYGON); 	// draw from bottom left, clockwise
 
 				glTexCoord2d(1.0, 1.0);
-				glVertex2f(x * tileWidth,				y * tileHeight);
-				
+				glVertex2f(x * tileWidth, y * tileHeight);
+
 				glTexCoord2d(1.0, 0.0);
-				glVertex2f(x * tileWidth,				y * tileHeight + tileHeight);
+				glVertex2f(x * tileWidth, y * tileHeight + tileHeight);
 
 				glTexCoord2d(0.0, 0.0);
-				glVertex2f(x * tileWidth + tileWidth,	y * tileHeight + tileHeight);
+				glVertex2f(x * tileWidth + tileWidth, y * tileHeight + tileHeight);
 
 				glTexCoord2d(0.0, 1.0);
-				glVertex2f(x * tileWidth + tileWidth,	y * tileHeight);
+				glVertex2f(x * tileWidth + tileWidth, y * tileHeight);
 				glEnd();
 
 				glDisable(GL_TEXTURE_2D);
 			}
-
 		}
 	}
 }
@@ -382,38 +615,38 @@ void initLevel() {
 	// levelMap encodes each tile as a character in a string
 	// key: - : empty space
 	//      # : ground tile
-	levelMap += "################################################################";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#---------------------------------------########---------------#";
-	levelMap += "#------------------------------------###-----------------------#";
-	levelMap += "#-------------------------###########--------------------------#";
-	levelMap += "#----------------------###-------------------------------------#";
-	levelMap += "#-------------#########----------------------------------------#";
-	levelMap += "#----------###-------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#---#########--------------------------------------------------#";
-	levelMap += "#------------##------------------------------------------------#";
-	levelMap += "#--------------############------------------------------------#";
-	levelMap += "#--------------------------###---------------------------------#";
-	levelMap += "#-----------------------------##-------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#---------------------------------######-----------------------#";
-	levelMap += "#-------------------------########------########---------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "#--------------------------------------------------------------#";
-	levelMap += "################################################################";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D----------------------------------------------#########-------D";
+	levelMap += "D------------------------------------------------------------##D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D---------------------------------------------------------###--D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D--------------------------------------------------------------D";
+	levelMap += "D----------------------------------------------#####-----------D";
+	levelMap += "D-----------------------------------#####----------------------D";
+	levelMap += "D-----------------------------------DDDDD----------------------D";
+	levelMap += "###############################################################D";
 
 	// reverses level map string so it is rendered the right way round
 	reverse(levelMap.begin(), levelMap.end());
@@ -443,7 +676,8 @@ void reshape(int width, int height)	{
 
 // initialise opengl window
 void init() {
-	
+	// add entities to vector
+	movingPlatforms.push_back(&platform1);
 }
 
 // processes key presses
@@ -454,8 +688,8 @@ void keyOperations() {
 
 	if (keyStates['w']) {
 		// check if player is already jumping
-		if (!playerEntity.onGround) {
-			playerEntity.onGround = true;
+		if (playerEntity.onGround) {
+			playerEntity.onGround = false;
 			playerEntity.velY = playerJumpRate;
 		}		
 	}
