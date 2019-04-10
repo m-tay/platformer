3367bb9281d97b374ca05e393f1e880694dcb885
M Taylor <ecstatic@gmail.com>
2019-04-10 12:54:53 +0100

    Adjusted Level 1 design

--- platformer/Game.h
+++ platformer/Game.h
@@ -14,7 +14,7 @@ class Enemy;
 class Game {
 public:
 	// DEBUG FLAG - set to true to view collision detection drawing 
-	bool debug = true;
+	bool debug = false;
 
 	// global parameters
 	int screenWidth = 900, screenHeight = 700;
